11debd5bdce90923e28408621dd1f9669a56cf99
Nik <43304085+CryFR@users.noreply.github.com>
2019-09-18 21:11:19 +0300

    Add files via upload

---
+++ PyDZ/num_pi.py
@@ -0,0 +1,5 @@
+import math
+def symbols(num):
+    pi = '{}'.format(math.pi)
+    print(pi[:num+2])
+symbols(35)
---
+++ PyDZ/str_n.py
@@ -0,0 +1,6 @@
+def caps(s, n):
+    if s > n:
+        print(s.upper)
+    else:
+        print(s)
+caps('Плачь', 4)
