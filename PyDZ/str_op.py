a2378449ac8a1d7435105f5ab38e1cdf499096e6
Nik <43304085+CryFR@users.noreply.github.com>
2019-10-03 01:09:59 +0300

    Delete str_op.py

--- PyDZ/str_op.py
+++
@@ -1,9 +0,0 @@
-s = "У лукоморья 123 дуб зеленый 456"
-if 'я' in s:
-    print(s.index('я', 1))
-print(s.count('у'))
-if s.isdigit():
-    print(s.upper())
-if len(s) > 4:
-    print(s.lower())
-print(s.replace(s[0], 'О'))
