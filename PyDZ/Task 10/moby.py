ff966f5469019d26df5b5d5194c611aa5be62180
Nik <43304085+CryFR@users.noreply.github.com>
2019-10-10 01:34:52 +0300

    Add files via upload

--- PyDZ/Task 10/moby.py
+++ PyDZ/Task 10/moby.py
@@ -1,7 +1,7 @@
 import string
 with open('moby.txt', 'r') as file:
-    lines = file.read().replace('\n', '')
-    reg_lines = lines.upper()
-    out_lines = "".join(x for x in reg_lines if x not in string.punctuation)
-with open('moby_clean.txt', 'w') as f:
-    f.write(out_lines.replace(" ", "\n"))
+    with open('moby_clean.txt', 'w') as f:
+        lines = file.read().replace('\n', '')
+        reg_lines = lines.upper()
+        out_lines = "".join(x for x in reg_lines if x not in string.punctuation)
+        f.write(out_lines.replace(" ", "\n"))
