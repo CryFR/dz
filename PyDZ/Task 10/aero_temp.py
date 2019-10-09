6ee52b7ae07d22bb91a17c5a39c41a08e0b4b496
Nik <43304085+CryFR@users.noreply.github.com>
2019-10-10 01:36:42 +0300

    Add files via upload

--- PyDZ/Task 10/aero_temp.py
+++ PyDZ/Task 10/aero_temp.py
@@ -1,12 +1,3 @@
-high = 0
-low = 0
-avg = 0
-unique = 0
 with open('temper.stat', 'r') as file:
     lines = [float(x) for x in [line.strip() for line in file]]
-high = max(lines)
-low = min(lines)
-avg = sum(lines) / len(lines)
-unique = len(set(lines))
-
-print(f" Максимальное значение: {high}\nМинимальное значение: {low}\nСредняя температура: {avg}\nКол-во уникальных температур: {unique}")
+print(f"Максимальное значение: {max(lines)}\nМинимальное значение: {min(lines)}\nСредняя температура: {sum(lines) / len(lines)}\nКол-во уникальных температур: {len(set(lines))}")
