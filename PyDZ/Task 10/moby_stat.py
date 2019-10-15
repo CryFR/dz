ed59b797397e97ea4f5f7ae640f8b37a6e8d0aa4
Nik <43304085+CryFR@users.noreply.github.com>
2019-10-15 21:11:59 +0300

    Add files via upload

--- PyDZ/Task 10/moby_stat.py
+++ PyDZ/Task 10/moby_stat.py
@@ -1,8 +1,11 @@
+import heapq
 with open('moby_clean.txt') as file:
     words = {}
     for word in file:
+        word = word.strip()
         if word not in words:
             words[word] = 1
         else:
             words[word] += 1
-print(word, words)
+    del words['']
+print(f"Пять самых частых слов: {heapq.nlargest(5, words, key=words.get)}\nПять самых редких слов: {heapq.nsmallest(5, words, key=words.get)}")
